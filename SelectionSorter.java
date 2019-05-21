a0a2ec9ba90899c8841d0fcbe23a101f6f5ffa03
bmoses00 <34358568+bmoses00@users.noreply.github.com>
2019-05-20 22:00:40 -0400

    Update SelectionSorter.java

--- SelectionSorter.java
+++ SelectionSorter.java
@@ -1,42 +1,20 @@
-/**
-  Implement a selection sort, in the Sorters structure
- */
-import java.util.ArrayList;
-
 public class SelectionSorter extends Sorter {
-
-    /**
-      Construct an instance to process the user's data
-     */
-    public SelectionSorter(  ArrayList< String> usersData) {
+    public SelectionSorter(ArrayList<String> usersData) {
         super(usersData);
     }
-    /*
-    PRECONDITION: Input is an ArrayList of strings
-    POSTCONDITION: ArrayList of Strings sorted in ascending order
-
-    For elements 0 to n - 2, swap the el with lowest element
-
-    */
-
-    public void mySort() { // selection sort
-        for (int counter = 0; counter < elements.size(); counter++) {
-            int index = champIndex(elements, counter);
-            String lowest = elements.get(index);
-            elements.set(index, elements.get(counter));
-            elements.set(counter, lowest);
-            // System.out.println(elements);
-        }
+    public void mySort() { 
+        for (counter = 0 --> elements.size()) 
+            elements.set(next, elements.set(
+             dweebIndex(next), elements.get(next)));
     }
-
-    private int champIndex(ArrayList<String> challengers, int startLocation) {
-         String dweeb = "ZZZZZZZZZZZZ"; // not a perfect solution, strings might be bigger
-         int dweebLocation = 0;
-         for (int index = startLocation; index < challengers.size(); index++)
-             if (challengers.get(index) != null && challengers.get(index).compareTo(dweeb) < 0) {
-                 dweeb = challengers.get(index);
-                 dweebLocation = index;
-             }
-         return dweebLocation;
+    private int dweebIndex(int startAt) {
+        int dweebAt = startAt;
+        Integer dweeb = elements.get(dweebAt);
+        for (testAt = startAt +1 --> elements.size())
+            if (elements.get(testAt).compareTo(dweeb) < 0) {
+                dweebAt = testAt;
+                dweeb = elements.get( dweebAt);
+            }
+        return dweebAt;
      }
 }
